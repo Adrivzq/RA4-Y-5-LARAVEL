855feb4a45012a8f83667fbee905cf0b71e7556b
Adrivzq <adristucom@gmail.com>
2026-03-29 19:29:38 +0200

    fix#11 api sin error

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-     Route::delete('actors/{id}', [ActorController::class, "destroy"])->name('destroy');
+    Route::delete('actors/{id}', [ActorController::class, "destroy"])->name('destroy');
     Route::get('films', [FilmController::class, "index"])->name('index');
