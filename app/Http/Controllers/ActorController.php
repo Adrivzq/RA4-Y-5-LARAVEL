341007d48157b79031338abd09b1cf7535dd576d
Adrivzq <adristucom@gmail.com>
2026-03-29 18:30:55 +0200

    fix#8 exercice FR1

---
+++ app/Http/Controllers/ActorController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+use App\Models\Actor;
+use Illuminate\Http\JsonResponse;
+
+
+class ActorController extends Controller
+{
+    //
+    public function listActors()
+    {
+        $title = "Listado de todos los actores";
+        $actors = Actor::all();
+       return view("actors.list", [ "actors" => $actors, "title" => $title]);
+    }
+    public function listActorsByDecade($year = null)
+    {
+        $title = "Buscar actores por criterio";
+
+        $start = $year;
+        $end = $year + 9;
+
+        $actors = Actor::whereYear('birthdate', '>=', $start)
+            ->whereYear('birthdate', '<=', $end)
+            ->get();
+
+        return view("actors.list", ["actors" => $actors, "title" => $title]);
+    }
+    public function countActors()
+    {
+        $title = "¿Cuántos actores hay?";
+        $actors = Actor::count();
+        return view("actors.counter", ["actors" => $actors, "title" => $title]);
+    }
+    public function destroy(Actor $id): JsonResponse
+    {
+         if ($id) {
+            $id->delete();
+            return response()->json([
+                "action" => "delete",
+                "status" => true
+            ], 200);
+        }
+        return response()->json([
+            "action" => "delete",
+            "status" => false
+        ], 404);
+    }
+  
+}
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,8 @@
 <?php
 
-use App\Http\Controllers\FilmController;
-use App\Http\Middleware\ValidateYear;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\FilmController;
+use App\Http\Controllers\ActorController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -22,20 +11,23 @@ Route::get('/', function () {
 Route::middleware('year')->group(function () {
     Route::group(['prefix' => 'filmout'], function () {
         // Routes included with prefix "filmout"
-        Route::get('oldFilms/{year?}', [FilmController::class, "listOldFilms"])->name('oldFilms');
-        Route::get('newFilms/{year?}', [FilmController::class, "listNewFilms"])->name('newFilms');
+        Route::get('oldFilms/{year}', [FilmController::class, "listOldFilms"])->name('oldFilms');
+        Route::get('newFilms/{year}', [FilmController::class, "listNewFilms"])->name('newFilms');
         Route::get('films/{year?}/{genre?}', [FilmController::class, "listFilms"])->name('listFilms');
-        Route::get('yearFilms/{year?}',  [FilmController::class, "listYearFilms"])->name('yearFilms');
+        Route::get('yearFilms/{year?}', [FilmController::class, "listYearFilms"])->name('yearFilms');
         Route::get('genreFilms/{genre?}', [FilmController::class, "listGenreFilms"])->name('genreFilms');
         Route::get('sortFilms/{year?}', [FilmController::class, "sortFilms"])->name('sortFilms');
         Route::get('countFilms/{year?}', [FilmController::class, "countFilms"])->name('countFilms');
     });
 });
+
 Route::middleware('url')->group(function () {
     Route::group(['prefix' => 'filmin'], function () {
         Route::post('createFilm', [FilmController::class, "createFilm"])->name('createFilm');
-        Route::post('isFilm', [FilmController::class, "isFilm"])->name(name: 'isFilm');
+        Route::post('isFilm', [FilmController::class, "isFilm"])->name('isFilm');
     });
 });
 
-
+Route::group(['prefix' => 'actorout'], function () {
+    Route::get('actors', [ActorController::class, 'listActors'])->name('listactors');
+});
